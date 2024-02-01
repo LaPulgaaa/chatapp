c71b76f54c1487b79044cc6f761c1e26c7ccdb32
Varun Singh <varunsingh9114@gmail.com>
2024-02-02 00:18:05 +0530

    frontend: UI changes and add recoil.

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,25 +1,32 @@
 'use client'
-
+import Message from "@/components/Message";
 import { Avatar, AvatarImage ,AvatarFallback} from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useEffect, useState } from "react"
-
-type RecievedMessage={
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { useRouter } from "next/navigation";
+export type RecievedMessage={
     type:string,
     payload:{
         roomId:string,
         message:string
     }
 }
+const imagesource="https://avatars.githubusercontent.com/u/123243429?v=4";
 
+const routerState=history.state;
 export default function Chat(){
     let ws:WebSocket;
+    const [room,setRoom]=useState("");
+    const router=useRouter();
     const [message,setMessage]=useState("");
     const [chat,setChat]=useState<RecievedMessage[]>([]);
     const [wsInstance,setWsInstance]=useState<WebSocket>();
     // console.log(chat);
     useEffect(()=>{
+        console.log();
+        setRoom(routerState.roomId);
          ws=new WebSocket("ws://localhost:3000");
          
         ws.onopen=function(){
@@ -27,48 +34,43 @@ export default function Chat(){
             const data={
                 type:"join",
                 payload:{
-                    roomId:"1",
+                    roomId:routerState.roomId.toString(),
                 }
             }
             ws.send(JSON.stringify(data));
             setWsInstance(ws);
         }
-
-        
-
-
         return ()=>{ws.close();}
     },[])
     if(wsInstance!=undefined)
-        {
-            wsInstance.onmessage=function(event){
-            const data=JSON.parse(event.data);
-            console.log(data) 
-            setChat([...chat,data]);
+    {
+         wsInstance.onmessage=function(event){
+         const data=JSON.parse(event.data);
+         console.log(data) 
+         setChat([...chat,data]);
         }
     }
     function sendMessage(){
         const data={
             type:"message",
             payload:{
-                roomId:"1",
+                roomId:room,
                 message
             }
         }
+        setMessage("")
         wsInstance!.send(JSON.stringify(data));
     }
 
     const chatComp=chat.map((data)=>{
-        return <div key={Math.floor(Math.random()*100000)} className="flex m-4">
-            <Avatar className="w-[30px] h-[30px]">
-                <AvatarImage src="https://avatars.githubusercontent.com/u/123243429?v=4" alt="gitProfile"/>
-                <AvatarFallback>VS</AvatarFallback>
-            </Avatar>
-            <p className=" border-l-2 pt-1 mr-2 italic ">{data.payload.message}</p>
-        </div>
+        return <Message data={data}/>
+            
     })
     
-    return <div className="w-[550px] m-32">
+    return <div className="h-full m-32">
+         <ScrollArea className="h-72 m-4 rounded-md border">
+            {chatComp}
+         </ScrollArea>
         <div className=" flex ">
         <Input 
         className="inline"
@@ -80,6 +82,7 @@ export default function Chat(){
         <Button  onClick={sendMessage} className=" ml-4">Send</Button>
         
     </div>
-    {chatComp}
+   
+    
     </div>
 }
