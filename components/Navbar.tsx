a40d188bac48bca28dc371222305ab3ab6d933b1
Varun Singh <varunsingh9114@gmail.com>
2024-02-08 03:57:34 +0530

    navbar: Show login or logout depending on token's availability.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,24 @@
 'use client'
 
+import { useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
+import { tokenState } from "@/lib/store/atom/Token";
 
 export default function Navbar(){
+    const resetToken=useResetRecoilState(tokenState)
     const router=useRouter();
+    const token=useRecoilValue(tokenState);
     return <div className="p-4 font-bold flex justify-between cursor-pointer">
         <h2 onClick={()=>router.push("/")}>chatcity</h2>
         <div className="flex justify-between">
-        <Button variant={"ghost"}>Login</Button>
-        <Button variant="ghost">Signup</Button>
+        {token!==undefined?<Button
+         onClick={()=>{
+            resetToken();
+            router.push("/")
+         }}
+         variant={"ghost"}>Logout</Button>:<Button variant="ghost">LogIn</Button>}
+        
         </div>
     </div>
 }
