9d4276f5c7939643b638d08cbc3a6496748de213
Varun Singh <varunsingh9114@gmail.com>
2024-02-13 01:57:50 +0530

    message: Add UI to display bot notice.

--- components/Message.tsx
+++ components/Message.tsx
@@ -4,12 +4,12 @@ import { userState } from "@/lib/store/atom/User"
 import { useRecoilValue } from "recoil"
 export default function Message({data}:{data:RecievedMessage}){
     const {name}=useRecoilValue(userState);
-    return <div key={Math.floor(Math.random()*100000)} className={`flex m-4  p-2 rounded-full ${data.payload.message.user===name?'justify-end':''}  `}>
+    return <div key={Math.floor(Math.random()*100000)} className={`flex m-4  p-2 rounded-full ${data.payload.message.user===name?'justify-end':data.payload.message.user==='mybot'?' justify-center':''}  `}>
     <Avatar className={`w-[35px] h-[35px] mr-2 bg-gray-200 mt-1 p-4 ${data.payload.message.user===name?'hidden':''}`}>
         
         <AvatarFallback>{data.payload.message.user?.substring(0,2)}</AvatarFallback>
     </Avatar>
-    <p className=" border-l-2 pb-2 bg-gray-100 mr-2 italic p-2 px-4 rounded-full">{data.payload.message.content}</p>
+    <p className={` border-l-2 pb-2 bg-gray-100 mr-2 italic p-2 px-4 rounded-full`}>{data.payload.message.content}</p>
     <Avatar className={`w-[35px] h-[35px] mr-2 bg-gray-200 mt-1 p-4 ${data.payload.message.user===name?'':'hidden'}`}>
         
         <AvatarFallback>{data.payload.message.user?.substring(0,2)}</AvatarFallback>
