270d524a1b06795e61f8e449c2aa65dce1481f74
Varun Singh <varunsingh9114@gmail.com>
2024-02-14 01:31:16 +0530

    routes: Create a 'login' endpoint.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -7,68 +7,71 @@ const router=express.Router();
 const prisma=new PrismaClient()
 
 
-router.post("/createUser",async (req,res)=>{
-    const {name,email,password}=req.body;
+// router.post("/createUser",async (req,res)=>{
+//     const {name,email,password}=req.body;
     
+//     try{
+//         const user=await prisma.user.create({
+//             data:{
+//                 name:name,
+//                 email:email,
+//                 password:password
+//             }
+//         })
+//         const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET!,{expiresIn:"3h"});
+//         console.log(token)
+//         res.status(201).json({
+//             message:"created new user",
+//             user,
+//             token
+//         })
+//     }catch(err)
+//     {
+//         res.status(400).send("internal server error.");
+//     }
+// })
+
+// router.get("/getUser",async(req,res)=>{
+//     console.log("get all users")
+//     try{
+//         const allUser=await prisma.user.findMany({});
+//         res.json({
+//             users:allUser
+//         })
+//     }catch(err)
+//     {
+//         res.status(404).send("could not find resource");
+//     }
+
+// })
+
+
+
+router.post("/login",async(req,res)=>{
+    const {username,password}=req.body;
+
     try{
-        const user=await prisma.user.create({
-            data:{
-                name:name,
-                email:email,
-                password:password
-            }
-        })
-        const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET!,{expiresIn:"3h"});
-        console.log(token)
-        res.status(201).json({
-            message:"created new user",
-            user,
-            token
-        })
-    }catch(err)
-    {
-        res.status(400).send("internal server error.");
-    }
-})
-
-router.get("/getUser",async(req,res)=>{
-    console.log("get all users")
-    try{
-        const allUser=await prisma.user.findMany({});
-        res.json({
-            users:allUser
-        })
-    }catch(err)
-    {
-        res.status(404).send("could not find resource");
-    }
-
-})
-
-router.get("/findUser/:email",async(req,res)=>{
-    const {email}=req.params;
-
-    try{
-        const user=await prisma.user.findFirst({
+        const member=await prisma.member.findFirst({
             where:{
-                email
+                username,
+                password
             }
         })
-        if(user!==undefined && user!==null)
+
+        if(member!==null)
         {
-            const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET!,{expiresIn:"3h"});
-            console.log(token)
+            const token=jwt.sign(member,process.env.ACCESS_TOKEN_SECRET!,{expiresIn:"3h"});
+
             res.status(200).json({
-                msg:"found user!!",
-                user,
+                msg:"Logged in Successfully!!",
+                member,
                 token
-            });
+            })
         }
         else
-        res.status(404).send("Not found!!");
-    }catch(err)
-    {
-        res.status(400).send("server error!!")
+        res.status(404).send("User not found!!");
+    }catch(err){
+        res.status(404).send("Could not find user!!")
     }
 })
 export default router;
