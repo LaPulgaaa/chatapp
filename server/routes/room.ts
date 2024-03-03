57c0763913496af9ad16f063a923720c2fadbad7
Varun Singh <varunsingh9114@gmail.com>
2024-03-03 15:58:09 +0530

    room: Add endpoint to get chats joined by a member.

--- server/routes/room.ts
+++ server/routes/room.ts
@@ -5,13 +5,39 @@ import { PrismaClient } from '@prisma/client';
 const router=express.Router();
 const prisma=new PrismaClient();
 
-type Chat={
+type CreateRoomSchema={
     name:string,
-    discription:string
+    discription:string,
+    memberId:string
 }
 
+router.get("/subscribedRoom/:memberId",async(req,res)=>{
+    const {memberId}=req.params;
+    try{
+        const message_subscribed_room=prisma.message.findMany({
+            where:{
+                content:`chat_${memberId}`
+            },
+            include:{
+                chat:true
+            }
+        })
+        res.status(200).json({
+            msg:"subscribed room found",
+            data:message_subscribed_room
+        })
+    }catch(err)
+    {
+        res.status(400).send("Server could not find out your rooms!")
+    }
+    
+})
+
 router.post("/createChat",async(req,res)=>{
-    const {name,discription}:Chat=req.body;
+    //The Message Schema serves as relational schema b/w Member and 
+    // Chat schema. We create chat table and the create a message row
+    // with a certain message to link Member and Chat model together .
+    const {name,discription,memberId}:CreateRoomSchema=req.body;
 
     try
     {
