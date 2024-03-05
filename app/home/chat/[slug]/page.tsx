af57f62bbf3c6cf1427d8ccc6e81c3fa01c75d80
Varun Singh <varunsingh9114@gmail.com>
2024-03-06 01:46:41 +0530

    slug: Fetch inbox messages and create UI for chat room.

--- app/home/chat/[slug]/page.tsx
+++ app/home/chat/[slug]/page.tsx
@@ -1,26 +1,48 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
-
-
-
+import type { ChatMessageData } from "@/packages/zod";
+import { ChatMessagesResponseSchema } from "@/packages/zod";
+import Inbox from "@/components/Inbox";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
 export default function Chat({params}:{params:{slug:string}}){
-    const [messages,setMessages]=useState();
+    const [messages,setMessages]=useState<ChatMessageData>();
+    console.log(messages)
     useEffect(()=>{
         async function fetch_messages(){
             try{
                 const resp=await fetch(`http://localhost:3000/chat/getMessage/${params.slug}`);
-                const data=await resp.json();
+                const {raw_data}=await resp.json();
+                const data=ChatMessagesResponseSchema.parse(raw_data);
                 // Parse this data using zod.
-                console.log(data)
+                setMessages(data);
+               
             }catch(err)
             {
                 alert(err);
+                console.log(err)
             }
         }
         fetch_messages();
     },[])
-    return <div>Soon you will able to chat here.</div>
+
+    
+    if(messages===undefined)
+    {
+        return <div>Loading....</div>
+    }
+
+    const InboxComponent=messages.messages.map((message)=>{
+        return <Inbox data={message}/>
+    })
+
+    return <div className="h-svh pb-24 ">
+        <ScrollArea id="chatbox" className="m-4 h-full rounded-md border ">
+            {InboxComponent}
+         </ScrollArea>
+        <div className="flex">
+        </div>
+    </div>
 
 }
