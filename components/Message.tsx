42ef70d8f51fad4f7485280192afcc17f4fe4f25
Varun Singh <varunsingh9114@gmail.com>
2024-03-11 00:28:48 +0530

    tailwind: Change border-radius and all border radius.

--- components/Message.tsx
+++ components/Message.tsx
@@ -4,13 +4,13 @@ import { userDetails } from "@/lib/store/atom/userDetails";
 import { useRecoilValue } from "recoil"
 export default function Message({data}:{data:RecievedMessage}){
     const {username}=useRecoilValue(userDetails);
-    return <div key={Math.floor(Math.random()*100000)} className={`flex m-4  p-2 rounded-full ${data.payload.message.user===username?'justify-end':data.payload.message.user==='mybot'?' justify-center':''}  `}>
-    <Avatar className={`w-[35px] h-[35px] mr-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'hidden':''}`}>
+    return <div key={Math.floor(Math.random()*100000)} className={`flex m-4 p-2 ${data.payload.message.user===username?'justify-end':data.payload.message.user==='mybot'?' justify-center':''}  `}>
+    <Avatar className={`w-[35px] h-[35px] mr-2 border-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'hidden':''}`}>
         
         <AvatarFallback>{data.payload.message.user?.substring(0,2)}</AvatarFallback>
     </Avatar>
-    <p className={` border-l-2 pb-2  mr-2 italic p-2 px-4 rounded-full`}>{data.payload.message.content}</p>
-    <Avatar className={`w-[35px] h-[35px] mr-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'':'hidden'}`}>
+    <p className={` border-l-2 pb-2 border-2  mr-2 italic p-2 px-4 rounded-lg text-wrap max-w-prose`}>{data.payload.message.content}</p>
+    <Avatar className={`w-[35px] h-[35px] mr-2 border-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'':'hidden'}`}>
         
         <AvatarFallback>{data.payload.message.user?.substring(0,2)}</AvatarFallback>
     </Avatar>
