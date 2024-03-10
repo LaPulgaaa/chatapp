efb45791052d62565b5b40e769cf1470ea69c874
Varun Singh <varunsingh9114@gmail.com>
2024-03-11 01:21:30 +0530

    message: Line wrap the message and thicken the border.

--- components/Inbox.tsx
+++ components/Inbox.tsx
@@ -1,15 +1,16 @@
 import { Avatar,AvatarFallback } from "./ui/avatar"
 import type { UnitMessage } from "@/packages/zod";
-
+import { userDetails } from "@/lib/store/atom/userDetails";
 import { useRecoilValue } from "recoil"
 export default function Inbox({data}:{data:UnitMessage}){
-    return <div key={data.id} className={`flex m-4 p-2 rounded-md ${data.sender.username==="LaPulgaa"?'justify-end':data.sender.username===''?' justify-center':''}  `}>
-    <Avatar className={`w-[35px] h-[35px] border-2 border-slate-400 mr-2 mt-1 p-4 ${data.sender.username==="LaPulgaa"?'hidden':''}`}>
+    const {username}=useRecoilValue(userDetails);
+    return <div key={data.id} className={`flex m-4 p-2  ${data.sender.username===username?'justify-end':data.sender.username===''?' justify-center':''}  `}>
+    <Avatar className={`w-[35px] h-[35px] border-2 border-slate-400 mr-2 mt-1 p-4 ${data.sender.username===username?'hidden':''}`}>
         
         <AvatarFallback>{data.sender.username?.substring(0,2)}</AvatarFallback>
     </Avatar>
-    <p className={` border-2 pb-2 mr-2 italic p-2 px-4 rounded-full`}>{data.content}</p>
-    <Avatar className={`w-[35px] h-[35px] border-2 border-slate-400  mr-2 mt-1 p-4 ${data.sender.username==="LaPulgaa"?'':'hidden'}`}>
+    <p className={` border-2 pb-2 mr-2 italic p-2 px-4 text-wrap max-w-prose rounded-md`}>{data.content}</p>
+    <Avatar className={`w-[35px] h-[35px] border-2 border-slate-400  mr-2 mt-1 p-4 ${data.sender.username===username?'':'hidden'}`}>
         
         <AvatarFallback>{data.sender.username?.substring(0,2)}</AvatarFallback>
     </Avatar>
