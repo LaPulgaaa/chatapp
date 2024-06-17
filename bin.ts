0493b5608386589b61cad64c688c3db787ac7f3a
Varun Singh <varunsingh9114@gmail.com>
2024-06-17 19:21:32 +0530

    express: Move `instance` to a separate module for testing convenience.

---
+++ bin.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import userRouter from "./server/routes/user";
+import chatRouter from "./server/routes/room";
+
+export const express_app=express();
+
+const corsOptions={
+    origin:"http://localhost:3000",
+    methods:'GET,PUT,POST,DELETE,PATCH',
+    credentials:true,
+
+}
+
+express_app.use(cors(corsOptions));
+express_app.use(cookieParser());
+express_app.use(express.json());
+express_app.use("/user",userRouter);
+express_app.use('/chat',chatRouter);
+
--- server.ts
+++ server.ts
@@ -1,13 +1,11 @@
-import express from 'express';
 import next from 'next';
 import http from 'http';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import userRouter from './server/routes/user';
-import chatRouter from './server/routes/room';
-import { start_worker } from './server/workers';
 import { WebSocketServer } from 'ws';
+
+import { start_worker } from './server/workers';
+import { express_app } from './bin';
 import {ws} from './server/socket/index'
+
 const port=3001;
 const hostname='localhost';
 const dev=process.env.NODE_ENV !== 'production';
@@ -15,25 +13,12 @@ const app=next({dev,hostname,port});
 
 const handle=app.getRequestHandler();
 
-export let express_app: express.Express;
-
 app.prepare().then(()=>{
-    express_app=express();
     const server=http.createServer(express_app);
     const wss=new WebSocketServer({server});
    
     ws(wss);
-    const corsOptions={
-        origin:"http://localhost:3000",
-        methods:'GET,PUT,POST,DELETE,PATCH',
-        credentials:true,
     
-    }
-    express_app.use(cors(corsOptions));
-    express_app.use(cookieParser());
-    express_app.use(express.json());
-    express_app.use("/user",userRouter);
-    express_app.use('/chat',chatRouter);
     express_app.all("*",(req,res)=>{
         return handle(req,res);
     })
