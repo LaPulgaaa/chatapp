6cdfc0bf36ed2546d193e0f6dc5e1d03d45f1ffd
Varun Singh <varunsingh9114@gmail.com>
2024-08-06 00:29:04 +0530

    sidebar: Fix join room dialog box not launching.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -24,15 +24,12 @@ export default function Sidebar(){
             <div className="text-center sm:text-left grid grid-cols-1 divide-y mr-1">
                 <div className="flex cursor-pointer my-[1/2] w-full hover:bg-gray-400 p-2 dark:hover:bg-gray-800 rounded-md ease-out duration-300 transition-all"><MessageCircleIcon/><p className="ml-2">Direct Messages</p></div>
                 <div className="flex cursor-pointer my-[1/2] w-full hover:bg-gray-400 p-2 dark:hover:bg-gray-800 rounded-md ease-out duration-300 transition-all"><UserIcon/><p className="ml-3">Set Status</p></div>
-                <div className="flex cursor-pointer my-[1/2] w-full hover:bg-gray-400 p-2 dark:hover:bg-gray-800 rounded-md ease-out duration-300 transition-all">
-                    <Dialog>
-                        <DialogTrigger>
-                            <div className="flex"><MessageSquareDotIcon/> <p className="ml-3">Join Room</p></div>
-                        </DialogTrigger>
+                <Dialog>
+                    <DialogTrigger>
+                        <div className="flex cursor-pointer my-[1/2] w-full hover:bg-gray-400 p-2 dark:hover:bg-gray-800 rounded-md ease-out duration-300 transition-all"><MessageSquareDotIcon/> <p className="ml-3">Join Room</p></div>
+                    </DialogTrigger>
                         <JoinRoomDialog/>
-                    </Dialog>
-                
-                </div>
+                </Dialog>
                 <div className="flex cursor-pointer my-[1/2] w-full hover:bg-gray-400 p-2 dark:hover:bg-gray-800 rounded-md ease-out duration-300 transition-all"
                 onClick={()=>router.push("/home/profile")}
                 >
