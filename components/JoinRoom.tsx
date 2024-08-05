648031b7dc4b6975276657d950d3e49246a206db
Varun Singh <varunsingh9114@gmail.com>
2024-08-06 00:20:37 +0530

    home: Prevent user from joining the same room twice.

--- components/JoinRoom.tsx
+++ components/JoinRoom.tsx
@@ -10,10 +10,18 @@ import { userDetails } from "@/lib/store/atom/userDetails";
 
 export default function JoinRoomDialog(){
     const [roomid,setRoomId]=useState<string>("");
-    const [room,setRoom]=useRecoilState(UserStateChats);
+    const [rooms,setRooms]=useRecoilState(UserStateChats);
     const profile_info=useRecoilValue(userDetails)
     const router=useRouter();
     async function searchRoom(){
+
+        const room_already_joined = rooms.find((r)=>r.id == roomid);
+        if(room_already_joined !== undefined)
+        {
+            router.push(`/home/chat/${roomid}`);
+            return;
+        }
+
         try{
             const resp=await fetch(`http://localhost:3001/chat/joinChat`,{
                 method:"POST",
@@ -26,18 +34,21 @@ export default function JoinRoomDialog(){
                 },
                 credentials:"include"
             });
+
             const raw_resp=await resp.json();
+
             if(resp.status===201)
             {
                 console.log("room found");
                 const room_info=raw_resp.raw_data;
-                setRoom([room_info,...room]);
+                setRooms([...rooms, room_info]);
                 router.push(`/home/chat/${room_info.id}`)
             }
             else
             {
                 alert("Room not found. Make sure the id is correct!!");
             }
+
         }catch(err){
             console.log(err);
             alert(err);
