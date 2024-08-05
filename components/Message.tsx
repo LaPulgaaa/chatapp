ede74d2d6263523ae74ebaf28b1950ce32e1ed0a
Varun Singh <varunsingh9114@gmail.com>
2024-08-05 21:53:52 +0530

    message: Show timestamp in message box.

--- components/Message.tsx
+++ components/Message.tsx
@@ -4,12 +4,16 @@ import { userDetails } from "@/lib/store/atom/userDetails";
 import { useRecoilValue } from "recoil"
 export default function Message({data}:{data:RecievedMessage}){
     const {username}=useRecoilValue(userDetails);
+    const curr_time = new Date();
     return <div key={Math.floor(Math.random()*100000)} className={`flex m-2 p-2 ${data.payload.message.user===username?'justify-end':data.payload.message.user==='pulgabot'?' justify-center':''}  `}>
     <Avatar className={`w-[35px] h-[35px] mr-2 border-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'hidden':''}`}>
         
         <AvatarFallback>{data.payload.message.user?.substring(0,2)}</AvatarFallback>
     </Avatar>
-    <p className={` border-l-2 pb-2 border-2  mr-2 italic p-2 px-4 rounded-lg text-wrap max-w-prose`}>{data.payload.message.content}</p>
+    <div className={` border-2 pb-1 mr-2 p-2  max-w-prose rounded-md flex`}>
+        <p className="italic text-wrap">{data.payload.message.content}</p>
+        <p className="flex justify-end text-[10px] mt-3 ml-2">{(curr_time.getHours() +":"+ curr_time.getMinutes())}</p>
+    </div>
     <Avatar className={`w-[35px] h-[35px] mr-2 border-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'':'hidden'}`}>
         
         <AvatarFallback>{data.payload.message.user?.substring(0,2)}</AvatarFallback>
