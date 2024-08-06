b72df56784364fe7e8832a1f58fd25aa82b39fd9
Varun Singh <varunsingh9114@gmail.com>
2024-08-06 18:13:47 +0530

    message: Fix minutes not displayed correctly in unack message.

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,10 +1,15 @@
+import { useRecoilValue } from "recoil"
+
 import { Avatar,AvatarFallback } from "./ui/avatar"
 import type { RecievedMessage } from "@/app/home/chat/[slug]/page"
 import { userDetails } from "@/lib/store/atom/userDetails";
-import { useRecoilValue } from "recoil"
+
 export default function Message({data}:{data:RecievedMessage}){
     const {username}=useRecoilValue(userDetails);
-    const curr_time = new Date();
+
+    let today_at = new Date().toTimeString().split(" ")[0];
+    let hour_min = today_at.split(":").slice(0, -1);
+
     return <div key={Math.floor(Math.random()*100000)} className={`flex m-2 p-2 ${data.payload.message.user===username?'justify-end':data.payload.message.user==='pulgabot'?' justify-center':''}  `}>
     <Avatar className={`w-[35px] h-[35px] mr-2 border-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'hidden':''}`}>
         
@@ -12,7 +17,7 @@ export default function Message({data}:{data:RecievedMessage}){
     </Avatar>
     <div className={` border-2 pb-1 mr-2 p-2  max-w-prose rounded-md flex`}>
         <p className="italic text-wrap">{data.payload.message.content}</p>
-        <p className="flex justify-end text-[10px] mt-3 ml-2">{(curr_time.getHours() +":"+ curr_time.getMinutes())}</p>
+        <p className="flex justify-end text-[10px] mt-3 ml-2">{(hour_min[0]+":"+ hour_min[1])}</p>
     </div>
     <Avatar className={`w-[35px] h-[35px] mr-2 border-2 border-slate-400 mt-1 p-4 ${data.payload.message.user===username?'':'hidden'}`}>
         
