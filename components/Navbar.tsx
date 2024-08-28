ff5a4d8e0ba9630e9f04caf83474b3b4815f7730
Varun Singh <varunsingh9114@gmail.com>
2024-08-28 22:21:03 +0530

    navbar: Reset recoil state on logout.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import { useResetRecoilState } from "recoil";
+
 import { Button } from "./ui/button"
 import { useRouter } from "next/navigation"
 import { useState } from "react";
 import * as React from "react";
 import { DarkLight } from "./DarkLight";
+import { userDetails } from "@/lib/store/atom/userDetails";
+import { UserStateChats } from "@/lib/store/atom/chats";
+
 export default function Navbar(){
     const router=useRouter();
     const [token,setToken]=useState(window.localStorage.getItem("token"));
+    const resetUserState = useResetRecoilState(userDetails);
+    const resetUserChats = useResetRecoilState(UserStateChats);
+
     return <div className="p-4 font-bold flex justify-between cursor-pointer">
         <h2 onClick={()=>router.push("/")}>chat.city</h2>
         <div className="flex justify-between">
@@ -16,6 +24,8 @@ export default function Navbar(){
          onClick={()=>{
             window.localStorage.clear()
             setToken(null);
+            resetUserChats();
+            resetUserState();
             router.push("/")
          }}
          variant={"ghost"}>Logout</Button>:<Button variant="ghost" onClick={()=>router.push("/login")}>LogIn</Button>}
