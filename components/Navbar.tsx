64575bc72d111ea08b286a98e7a22829d1b6753e
Varun Singh <varunsingh9114@gmail.com>
2024-09-05 16:50:51 +0530

    Revert commit adding recoil selector to fetch userdetails.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,43 @@
-'use client'
-
-import { useResetRecoilState } from "recoil";
-
-import { Button } from "./ui/button"
-import { useRouter } from "next/navigation"
-import { useState } from "react";
+import Link from "next/link";
 import * as React from "react";
+
+import { redirect } from "next/navigation";
+
+import { Button } from "./ui/button";
 import { DarkLight } from "./DarkLight";
+
 import { userDetails } from "@/lib/store/atom/userDetails";
-import { UserStateChats } from "@/lib/store/atom/chats";
-import { Signal } from "@/app/home/signal";
+import { useRecoilState, useResetRecoilState } from "recoil";
 
-export default function Navbar(){
-    const router=useRouter();
-    const [token,setToken]=useState(window.localStorage.getItem("token"));
-    const resetUserState = useResetRecoilState(userDetails);
-    const resetUserChats = useResetRecoilState(UserStateChats);
-
-    return <div className="p-4 font-bold flex justify-between cursor-pointer">
-        <h2 onClick={()=>router.push("/")}>chat.city</h2>
-        <div className="flex justify-between">
-        {token=="valid"?<Button
-        className="mx-2"
-         onClick={()=>{
-            Signal.get_instance().CLOSE();
-            window.localStorage.clear()
-            setToken(null);
-            resetUserChats();
-            resetUserState();
-            router.push("/");
-         }}
-         variant={"ghost"}>Logout</Button>:<Button variant="ghost" onClick={()=>router.push("/login")}>LogIn</Button>}
-        {token===null?<Button
-        className="px-2 mx-6"
-        variant={"ghost"}
-         onClick={()=>{
-            router.push("/signup")
-        }}>Signup</Button>:''}
-        <DarkLight/>
-        </div>
+export default function Navbar(){;
+    const [userdetails,setUserdetails] = useRecoilState(userDetails);
+    const clearUserDetails = useResetRecoilState(userDetails);
+    return (
+        <div className="p-4 font-bold flex justify-between cursor-pointer">
+            <Link href="/">
+                <h2>chat.city</h2>
+            </Link>
+            <div className="flex justify-between">
+                {
+                    userdetails.id && 
+                    <Link href={"/"}>
+                        <Button 
+                        onClick={()=>{
+                            clearUserDetails();
+                            redirect("/login")
+                        }}
+                        className="mx-2" variant={"ghost"}>Logout</Button>
+                    </Link>
+                }
+                {
+                    !userdetails.id && <div>
+                        <Link href={"/login"}><Button variant="ghost">LogIn</Button></Link>
+                        <Link href={"/signup"}><Button className="px-2 mx-6" variant={"ghost"}>Signup</Button></Link>
+                    </div>
+                }
+                <DarkLight/>
+            </div>
         
-    </div>
+        </div>
+    )
 }
