749b7c49dd6a99596a73c2b48a60c2990eb99076
Varun Singh <varunsingh9114@gmail.com>
2024-10-19 19:14:54 +0530

    home: Mute secondary text in home page tabs.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -144,7 +144,7 @@ const RoomTabs = memo(
                             <p className="hidden md:block">{get_last_msg_time(convo.lastmsgAt)}</p>
                             </div>
 
-                            <p className="border-l-2 pl-6 italic">{convo.discription}</p>
+                            <p className="border-l-2 pl-6 italic text-muted-foreground">{convo.discription}</p>
                         </div>
                     }
                     else {
@@ -160,7 +160,7 @@ const RoomTabs = memo(
                                 <p className="hidden md:block">{get_last_msg_time(convo.lastmsgAt)}</p>
                                 </div>
 
-                                <p className="border-l-2 pl-6 italic">{convo.messages[0]?.content}</p>
+                                <p className="border-l-2 pl-6 italic text-muted-foreground">{convo.messages[0]?.content}</p>
                             </div>
                         )
                     }
