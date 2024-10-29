09768acc4ea383a2e3a351690d85d67a2d75a246
Varun Singh <varunsingh9114@gmail.com>
2024-10-29 18:34:26 +0530

    middleware: Remove server middleware for now.

--- server/middleware/authenticate.ts
+++ server/middleware/authenticate.ts
@@ -2,11 +2,5 @@ import { Request,Response,NextFunction } from 'express';
 
 
 export default async function authenticate(req:Request,res:Response,next:NextFunction){
-    const token=req.cookies["next-auth.session-token"];
-    if(!token){
-        res.status(401).json({
-            message: "unauthorized access. token not present"
-        })
-    };
     next();
 }
