855c9c03f0c2087e932a469ccf9f45c2ed82bf37
Varun Singh <varunsingh9114@gmail.com>
2024-11-19 22:40:38 +0530

    dm: Add api route to sweep new direct messages.
    We use recoil to cache message history. So, when a new message is
    sent by a user, the chat is left and returned to, the new message is
    no longer visible because the recoil shows the old cached value
    and react state containing broadcasted message is cleared. Hence, we
    we create a sweeper route to fetch new messages (ie. messages with id
    greater than the most recent message in recoil cache.
    
    This is a more efficient approach since we don't fetch entire message
    history on every mount.

---
+++ app/api/message/dm/[slug]/route.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@/packages/prisma/prisma_client";
+import { getToken } from "next-auth/jwt";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest,{ params }:{ params: { slug: string }}){
+    const conc_id = params.slug;
+    const token = await getToken({ req });
+    const {last_msg_id}:{ last_msg_id: number } = await req.json();
+
+    if(token == null){
+        return Response.json({
+            message: "UNAUTHORISED_ACCESS"
+        },{ status: 401 });
+    }
+
+    try{
+        const dms = await prisma.directMessage.findMany({
+            where: {
+                connectionId: conc_id,
+                id: {
+                    gt: last_msg_id
+                },
+            },
+            select: {
+                id: true,
+                createdAt: true,
+                content: true,
+                sendBy:{
+                    select: {
+                        username: true,
+                    }
+                }
+            }
+            
+        });
+
+        return Response.json({
+            message: "SUCCESS",
+            data: dms,
+        },{ status: 200 });
+    }catch(err){
+        console.log(err);
+        return Response.json({
+            message: "SERVER_ERROR",
+        },{ status: 500 });
+    }
+}
