e3444f7ee59a61bbc6f99532bb2ea9074e1f0065
Varun Singh <varunsingh9114@gmail.com>
2024-11-25 01:01:12 +0530

    chat: Update recoil cache with updated last sent msg for chats.

---
+++ lib/store/atom/subscribed_chats_state.ts
@@ -0,0 +1,8 @@
+import { atom } from "recoil";
+import { ChatReponse } from "@/packages/zod";
+import { fetch_user_chats } from "../selector/fetch_chats";
+
+export const subscribed_chats_state = atom<ChatReponse>({
+    key: "subscribed_chats_state",
+    default: fetch_user_chats
+})
