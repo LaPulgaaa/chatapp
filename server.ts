c41d2bf74632fe42218a31ec8896ac6d46aa4b22
Varun Singh <varunsingh9114@gmail.com>
2024-11-25 00:02:54 +0530

    server: Remove custom server.

--- server.ts
+++ server.ts
@@ -1,31 +1,26 @@
-import next from 'next';
 import http from 'http';
 import { WebSocketServer } from 'ws';
 
 import { start_worker } from './server/workers';
 import { express_app } from './bin';
-import {ws} from './server/socket/index'
+import { ws } from './server/socket/index'
 
 const port=3001;
-const hostname='localhost';
-const dev=process.env.NODE_ENV !== 'production';
-const app=next({dev,hostname,port});
 
-const handle=app.getRequestHandler();
-
-app.prepare().then(()=>{
-    const server=http.createServer(express_app);
-    const wss=new WebSocketServer({server});
-   
-    ws(wss);
+function main() {
+    try{
+        const server=http.createServer(express_app);
+        const wss=new WebSocketServer({server});
     
-    express_app.all("*",(req,res)=>{
-        return handle(req,res);
-    })
-    server.listen(port,()=>{
-        console.log("listening on port 3001, ws server connected!");
-    })
-    start_worker();
-}).catch((err:Error)=>{
-    console.log(err);
-})
+        ws(wss);
+        
+        server.listen(port,()=>{
+            console.log("listening on port 3001, ws server connected!");
+        })
+        start_worker();
+    }catch(err){
+        console.log(err);
+    }
+}
+
+main();
