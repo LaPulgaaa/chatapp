ee2ea47cbb8e40451dd025ee9da48ef017d1192f
Varun Singh <varunsingh9114@gmail.com>
2025-02-04 03:10:14 +0530

    msg_connect: Add invite callback on mount.

--- app/(message)/msg_connect.tsx
+++ app/(message)/msg_connect.tsx
@@ -4,11 +4,13 @@ import assert from "minimalistic-assert";
 
 import { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRecoilStateLoadable } from "recoil";
+import { useRecoilRefresher_UNSTABLE, useRecoilStateLoadable } from "recoil";
 import { Signal } from "../home/signal";
 import { MessageDeletePayload, MessagePinPayload, MessageStarPayload } from "@/packages/zod";
 import { direct_msg_state } from "@/lib/store/atom/dm";
 import { subscribed_chats_state } from "@/lib/store/atom/subscribed_chats_state";
+import { useToast } from "@/hooks/use-toast";
+import { fetch_dms } from "@/lib/store/selector/fetch_dms";
 
 type DeleteMsgCallbackData = {
     type: string,
@@ -39,8 +41,20 @@ export type UpdateDetailsData = {
 
 export default function Connect(){
     const session = useSession();
+    const { toast } = useToast();
     const [dms,setDms] = useRecoilStateLoadable(direct_msg_state);
     const [roomsStateData,setRoomsStateData] = useRecoilStateLoadable(subscribed_chats_state);
+    const refresh_dms = useRecoilRefresher_UNSTABLE(fetch_dms);
+
+    function recieve_invite_callback(raw_data: string){
+        const data = JSON.parse(raw_data);
+        toast({
+            title: data.payload.requestBy,
+            description: data.payload.content,
+        });
+        refresh_dms();
+    }
+
 
     function delete_msg_callback(raw_string: string){
         const data:DeleteMsgCallbackData = JSON.parse(`${raw_string}`);
@@ -157,6 +171,7 @@ export default function Connect(){
     useEffect(()=>{
         if(session.status === "authenticated"){
             //@ts-ignore
+            Signal.get_instance(session.data.username).REGISTER_CALLBACK("INVITE",recieve_invite_callback);
             Signal.get_instance().REGISTER_CALLBACK("DELETE_NON_ECHO",delete_msg_callback);
             Signal.get_instance().REGISTER_CALLBACK("PIN_MSG_CALLBACK_NON_ECHO",pin_msg_callback);
             Signal.get_instance().REGISTER_CALLBACK("STARRED_NON_ECHO_CALLBACK",star_msg_callback);
@@ -167,7 +182,8 @@ export default function Connect(){
             if(session.status === "authenticated")
             {
                 //@ts-ignore
-                Signal.get_instance(session.data.username).DEREGISTER("DELETE_NON_ECHO");
+                Signal.get_instance(session.data.username).DEREGISTER("INVITE");
+                Signal.get_instance().DEREGISTER("DELETE_NON_ECHO");
                 Signal.get_instance().DEREGISTER("PIN_MSG_CALLBACK_NON_ECHO");
                 Signal.get_instance().DEREGISTER("STARRED_NON_ECHO_CALLBACK");
                 Signal.get_instance().DEREGISTER("UPDATE_DETAILS_CALLBACK");
