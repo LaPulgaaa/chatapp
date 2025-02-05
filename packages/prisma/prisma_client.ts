4489afe0a603c89fab7854dd82bd128451d3a01d
Varun Singh <varunsingh9114@gmail.com>
2025-02-06 02:40:46 +0530

    packages: Format code using prettier.

--- packages/prisma/prisma_client.ts
+++ packages/prisma/prisma_client.ts
@@ -4,19 +4,18 @@ import { PrismaClient } from "@prisma/client";
 // has only instance globally. This function is single point of access
 // to that object.
 // https://chatgpt.com/c/d61d1539-fca3-4025-8129-7ea993a8565d
-const prisma_client_singleton=()=>{
-    return new PrismaClient();
-}
+const prisma_client_singleton = () => {
+  return new PrismaClient();
+};
 
-type PrismaClientSingleton=ReturnType<typeof prisma_client_singleton>;
+type PrismaClientSingleton = ReturnType<typeof prisma_client_singleton>;
 
-let globalPrisma=globalThis as unknown as {prisma: PrismaClientSingleton | undefined}; //this is the type dec.
+let globalPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+}; //this is the type dec.
 
 export let prisma = globalPrisma.prisma ?? prisma_client_singleton(); // checking if client is cached.
 
-if(process.env.NODE_ENV!=="production"){
-    globalPrisma.prisma=prisma; //caching the client
+if (process.env.NODE_ENV !== "production") {
+  globalPrisma.prisma = prisma; //caching the client
 }
-
-
-
--- packages/zod.ts
+++ packages/zod.ts
@@ -1,185 +1,204 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 export const user_signup_form_schema = z.object({
-    username: z.string({
-        required_error: "Username is required"
-    }).min(8,{message: "username must atleast be 8 characters"}).max(20),
-    email: z.string().email({message: "Invalid email address"}),
-    password: z.string({
-        required_error: "Password is required"
-    }).min(10,{message: "Password should atleast be 10 characters"}).max(14)
-})
+  username: z
+    .string({
+      required_error: "Username is required",
+    })
+    .min(8, { message: "username must atleast be 8 characters" })
+    .max(20),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(10, { message: "Password should atleast be 10 characters" })
+    .max(14),
+});
 
+export const join_schema = z.object({
+  username: z
+    .string({
+      required_error: "Username is required",
+    })
+    .min(8, { message: "username must atleast be 8 characters" })
+    .max(20),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(10, { message: "Password should atleast be 10 characters" })
+    .max(14),
+});
 
-export const join_schema=z.object({
-    username: z.string({
-        required_error: "Username is required"
-    }).min(8,{message: "username must atleast be 8 characters"}).max(20),
-    password: z.string({
-        required_error: "Password is required"
-    }).min(10,{message: "Password should atleast be 10 characters"}).max(14)
-})
+export type Join = z.infer<typeof user_signup_form_schema>;
 
-export type Join=z.infer<typeof user_signup_form_schema>;
+export const message_schema = z.object({
+  roomId: z.string({
+    required_error: "no room to chat with!",
+  }),
+  username: z.string({
+    required_error: "user unavailable",
+  }),
+  message: z
+    .string()
+    .max(50, { message: "can not send message of length more than 50." }),
+});
 
+export type Message = z.infer<typeof message_schema>;
 
-export const message_schema=z.object({
-    roomId:z.string({
-        required_error:"no room to chat with!"
-    }),
-    username:z.string({
-        required_error:"user unavailable"
-    }),
-    message:z.string().max(50,{message:"can not send message of length more than 50."})
-})
+export const room_details_schema = z.object({
+  name: z
+    .string({
+      required_error: "enter a valid name",
+    })
+    .min(1),
+  discription: z
+    .string({
+      required_error: "discription should be atleast 1 character.",
+    })
+    .min(6)
+    .max(50),
+});
 
-export type Message=z.infer<typeof message_schema>;
-
-export const room_details_schema=z.object({
-    name:z.string({
-        required_error:"enter a valid name"
-    }).min(1),
-    discription:z.string({
-        required_error:"discription should be atleast 1 character."
-    }).min(6).max(50)
-})
-
-export type RoomType=z.infer<typeof room_details_schema>;
+export type RoomType = z.infer<typeof room_details_schema>;
 
 export const user_chat_schema = z.object({
-    createdAt:z.string(),
-    lastmsgAt: z.string(),
-    deleted:z.boolean(),
-    discription:z.string(),
-    id:z.string(),
-    name:z.string(),
-    conn_id: z.number(),
-    messages: z.array(z.object({
-        id: z.number(),
-        content: z.string(),
-        sender: z.object({
-            username: z.string(),
-            name: z.string().nullish(),
-        }),
-        createdAt: z.string(),
-    })),
-    draft: z.string().optional(),
-})
+  createdAt: z.string(),
+  lastmsgAt: z.string(),
+  deleted: z.boolean(),
+  discription: z.string(),
+  id: z.string(),
+  name: z.string(),
+  conn_id: z.number(),
+  messages: z.array(
+    z.object({
+      id: z.number(),
+      content: z.string(),
+      sender: z.object({
+        username: z.string(),
+        name: z.string().nullish(),
+      }),
+      createdAt: z.string(),
+    }),
+  ),
+  draft: z.string().optional(),
+});
 
 export type UserChat = z.output<typeof user_chat_schema>;
 
-export const user_chats_response_schema=z.array(
-    user_chat_schema, 
-)
+export const user_chats_response_schema = z.array(user_chat_schema);
 
-export const unit_message_schema= z.object({
-    content:z.string(),
-    createdAt:z.string(),
-    id:z.number(),
-    sender:z.object({
-        username:z.string(),
-        name: z.string().nullish(),
-    })
-})
+export const unit_message_schema = z.object({
+  content: z.string(),
+  createdAt: z.string(),
+  id: z.number(),
+  sender: z.object({
+    username: z.string(),
+    name: z.string().nullish(),
+  }),
+});
 
 export const chat_messages_schema = z.array(unit_message_schema);
 
-export const chat_messages_response_schema=z.object({
-    messages: chat_messages_schema,
+export const chat_messages_response_schema = z.object({
+  messages: chat_messages_schema,
 });
 
 export const room_details_response_schema = chat_messages_response_schema;
-export type ChatMessageData=z.infer<typeof chat_messages_response_schema>;
-export type UnitMessage=z.infer<typeof unit_message_schema>;
+export type ChatMessageData = z.infer<typeof chat_messages_response_schema>;
+export type UnitMessage = z.infer<typeof unit_message_schema>;
 
-export type ChatReponse=z.infer<typeof user_chats_response_schema>;
+export type ChatReponse = z.infer<typeof user_chats_response_schema>;
 
-export const member_profile_schema=z.object({
-    username:z.string(),
-    name: z.string().optional(),
-    about:z.string().optional(),
-    favorite:z.array(z.string()),
-    status:z.string(),
-    avatarurl:z.string().optional(),
-})
+export const member_profile_schema = z.object({
+  username: z.string(),
+  name: z.string().optional(),
+  about: z.string().optional(),
+  favorite: z.array(z.string()),
+  status: z.string(),
+  avatarurl: z.string().optional(),
+});
 
-export type MemberProfile=z.infer<typeof member_profile_schema>;
+export type MemberProfile = z.infer<typeof member_profile_schema>;
 
-export const worker_payload = z.discriminatedUnion("type",[
-    z.object({
-        type: z.literal("chat"),
-        content: z.string(),
-        chatId: z.string(),
-        createdAt: z.string(),
-        memberId: z.string(),
-    }),
-    z.object({
-        type: z.literal("dm"),
-        content: z.string(),
-        concId: z.string(),
-        createdAt: z.string(),
-        friendshipId: z.string(),
-        sender: z.string(),
-        hash: z.string(),
-    })
-])
+export const worker_payload = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("chat"),
+    content: z.string(),
+    chatId: z.string(),
+    createdAt: z.string(),
+    memberId: z.string(),
+  }),
+  z.object({
+    type: z.literal("dm"),
+    content: z.string(),
+    concId: z.string(),
+    createdAt: z.string(),
+    friendshipId: z.string(),
+    sender: z.string(),
+    hash: z.string(),
+  }),
+]);
 
 //z.output is an alias for z.infer
-export type WorkerPayload=z.output<typeof worker_payload>;
+export type WorkerPayload = z.output<typeof worker_payload>;
 
 export const room_header_details = z.object({
-    name: z.string(),
-    discription: z.string(),
-    createdAt: z.string()
+  name: z.string(),
+  discription: z.string(),
+  createdAt: z.string(),
 });
 
 export type RoomHeaderDetails = z.output<typeof room_header_details>;
 
-export const room_member_details_schema = z.array(z.object({
+export const room_member_details_schema = z.array(
+  z.object({
     username: z.string(),
     name: z.string().nullish(),
     active: z.boolean(),
     avatarurl: z.string().nullish(),
     status: z.string().nullish(),
-}))
+  }),
+);
 
 export type RoomMemberDetails = z.output<typeof room_member_details_schema>;
 
 export const user_details_edit_form_schema = z.object({
-    username: z.string(),
-    name: z.string().optional(),
-    avatarurl: z.string().optional(),
-    about: z.string().optional(),
-    status: z.string().optional(),
-})
+  username: z.string(),
+  name: z.string().optional(),
+  avatarurl: z.string().optional(),
+  about: z.string().optional(),
+  status: z.string().optional(),
+});
 
 export const direct_msg_schema = z.object({
-    id: z.number(),
-    content: z.string(),
-    createdAt: z.string(),
-    sendBy: z.object({
-        username: z.string(),
-        name: z.string().nullish(),
-    }),
-    pinned: z.boolean(),
-    starred: z.array(z.string()),
+  id: z.number(),
+  content: z.string(),
+  createdAt: z.string(),
+  sendBy: z.object({
+    username: z.string(),
+    name: z.string().nullish(),
+  }),
+  pinned: z.boolean(),
+  starred: z.array(z.string()),
 });
 
 export const private_chat_schema = z.object({
-    id: z.string(),
-    blocked: z.boolean(),
-    lastmsgAt: z.string(),
-    to: z.object({
-        username: z.string(),
-        avatarurl: z.string().nullable(),
-        about: z.string().nullable(),
-        favorite: z.array(z.string()),
-        name: z.string().nullish(),
-        status: z.string().nullable(),
-    }),
-    messages: z.array(direct_msg_schema),
-    connectionId: z.string(),
-    draft: z.string().optional(),
+  id: z.string(),
+  blocked: z.boolean(),
+  lastmsgAt: z.string(),
+  to: z.object({
+    username: z.string(),
+    avatarurl: z.string().nullable(),
+    about: z.string().nullable(),
+    favorite: z.array(z.string()),
+    name: z.string().nullish(),
+    status: z.string().nullable(),
+  }),
+  messages: z.array(direct_msg_schema),
+  connectionId: z.string(),
+  draft: z.string().optional(),
 });
 
 export const private_chats_schema = z.array(private_chat_schema);
@@ -189,43 +208,45 @@ export type PrivateChat = z.output<typeof private_chat_schema>;
 export type PrivateChats = PrivateChat[];
 
 export type DirectMsg = {
-    id: number,
-    content: string,
-    createdAt: Date,
-    sendBy: {
-        username: string,
-    },
-    pinned: boolean,
-    starred: string[],
-}
+  id: number;
+  content: string;
+  createdAt: Date;
+  sendBy: {
+    username: string;
+  };
+  pinned: boolean;
+  starred: string[];
+};
 
-export const friend_search_result_schema = z.discriminatedUnion("is_friend", [
+export const friend_search_result_schema = z
+  .discriminatedUnion("is_friend", [
     z.object({
-        is_friend: z.literal(false),
-        friendship_data: z.undefined(),
+      is_friend: z.literal(false),
+      friendship_data: z.undefined(),
     }),
     z.object({
-        is_friend: z.literal(true),
-        friendship_data: z.object({
-            id: z.string(),
-            connectionId: z.string(),
-            messageFrom: z.string().optional(),
-            blocked: z.boolean(),
-            messages: z.array(direct_msg_schema),
-            is_active: z.boolean(),
-        })
-    })
-]).and(
+      is_friend: z.literal(true),
+      friendship_data: z.object({
+        id: z.string(),
+        connectionId: z.string(),
+        messageFrom: z.string().optional(),
+        blocked: z.boolean(),
+        messages: z.array(direct_msg_schema),
+        is_active: z.boolean(),
+      }),
+    }),
+  ])
+  .and(
     z.object({
-            profile_info: z.object({
-                status: z.string().nullable(),
-                about: z.string().nullable(),
-                favorite: z.array(z.string()),
-                name: z.string().nullish(),
-                avatarurl: z.string().nullable(),
-            })
-    })
-);
+      profile_info: z.object({
+        status: z.string().nullable(),
+        about: z.string().nullable(),
+        favorite: z.array(z.string()),
+        name: z.string().nullish(),
+        avatarurl: z.string().nullable(),
+      }),
+    }),
+  );
 
 export type FriendSearchResult = z.output<typeof friend_search_result_schema>;
 
@@ -234,25 +255,26 @@ export type DmProfileInfo = FriendSearchResult["profile_info"];
 export type DirectMessage = z.output<typeof direct_msg_schema>;
 
 export const message_delete_payload = z.object({
-    hash: z.string(),
-    conc_id: z.string(),
-    type: z.literal("DM"),
-    id: z.number(),
-})
+  hash: z.string(),
+  conc_id: z.string(),
+  type: z.literal("DM"),
+  id: z.number(),
+});
 
 export type MessageDeletePayload = z.output<typeof message_delete_payload>;
 
 export const message_pin_payload = z.object({
-    type: z.enum(["DM","CHAT"]),
-    pinned: z.boolean(),
-    hash: z.string(),
-    id: z.number(),
-    sender_id: z.string(),
-    conc_id: z.string(),
-})
+  type: z.enum(["DM", "CHAT"]),
+  pinned: z.boolean(),
+  hash: z.string(),
+  id: z.number(),
+  sender_id: z.string(),
+  conc_id: z.string(),
+});
 
-export const message_star_payload = message_pin_payload.omit({pinned: true}).
-                                    and(z.object({ starred: z.array(z.string()) }));
+export const message_star_payload = message_pin_payload
+  .omit({ pinned: true })
+  .and(z.object({ starred: z.array(z.string()) }));
 
 export type MessagePinPayload = z.output<typeof message_pin_payload>;
 export type MessageStarPayload = z.output<typeof message_star_payload>;
