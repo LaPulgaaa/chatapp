b98edaaba342c74c81175811d452f3727b53bb20
Varun Singh <varunsingh9114@gmail.com>
2025-04-25 00:42:52 +0530

    schema: Add default value for coloumn description for backward comp.

--- packages/prisma/migrations/20250424192001_rename_coloumn_description/migration.sql
+++ packages/prisma/migrations/20250424192001_rename_coloumn_description/migration.sql
@@ -2,9 +2,8 @@
   Warnings:
 
   - You are about to drop the column `discription` on the `Chat` table. All the data in the column will be lost.
-  - Added the required column `description` to the `Chat` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "Chat" DROP COLUMN "discription",
-ADD COLUMN     "description" TEXT NOT NULL;
+ADD COLUMN     "description" TEXT NOT NULL DEFAULT '';
