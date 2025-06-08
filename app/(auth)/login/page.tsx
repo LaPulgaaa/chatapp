1f93173ad2c764c452eecfd8e28993e4eea14b3b
Varun Singh <varunsingh9114@gmail.com>
2025-06-08 17:32:53 +0530

    login: Use react-form-hook for login form.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { valibotResolver } from "@hookform/resolvers/valibot";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { signIn, useSession } from "next-auth/react";
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect} from "react";
+import { useForm } from "react-hook-form";
+import type * as v from "valibot";
 
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
@@ -16,16 +19,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { ToastAction } from "@/components/ui/toast";
 import { toast } from "@/hooks/use-toast";
+import { user_login_form_schema } from "@/packages/valibot";
 import GoogleLogoIcon from "@/public/icons8-google-logo-48.png";
+
+
+type FormValue = v.InferOutput<typeof user_login_form_schema>;
 
 export default function Login() {
   const session = useSession();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
 
   // a little shady
@@ -33,12 +45,29 @@ export default function Login() {
     if (session.status === "authenticated") router.push("/home");
   }, [router, session.status]);
 
-  async function joinRoom() {
+  const form = useForm<FormValue>({
+    resolver: valibotResolver(user_login_form_schema),
+    defaultValues: {
+      password: "",
+      email: "",
+    },
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting, isDirty, isLoading },
+  } = form;
+
+
+  async function onSubmit(values: FormValue) {
     try {
+
       const credentials = {
-        email,
-        password,
+        email: values.email,
+        password: values.password,
       };
+
       const resp = await signIn<"credentials">("credentials", {
         ...credentials,
         redirect: false,
@@ -57,7 +86,7 @@ export default function Login() {
     } catch (err) {
       console.log(err);
       toast({
-        title: "Error while Signing in",
+        title: "Error while signing in user!",
         variant: "destructive",
       });
     }
@@ -66,60 +95,74 @@ export default function Login() {
   return (
     <div className="h-screen flex flex-col">
       <Navbar />
-      <div className="flex-1 flex items-center justify-center">
-        <div className="flex flex-col"><h2 className="flex justify-center">
-          Welcome Back !
+      <div className="flex-1 flex items-center justify-center ">
+        <div className="flex flex-col">
+          {/* register form */}
+        <h2 className="flex justify-center">
+          Welcome back!
         </h2>
+
         <Card className="sm:w-[500px] divide-y space-y-2">
-        <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>
-                Fill in the details to join your peers!{" "}
-              </CardDescription>
+            <CardHeader>
+              <CardTitle className="truncate">SignIn and get back to chatting!</CardTitle>
+              <CardDescription>Your peers are waiting for you!</CardDescription>
             </CardHeader>
-            <CardContent>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                    value={email}
-                    id="email"
-                    placeholder="johndoe123@gmail.com"
+            <Form {...form}>
+              <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 p-4">
+                <CardContent>
+                  <FormField
+                    control={control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input type="text" placeholder="johndoe@example.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    type="password"
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                    value={password}
-                    id="password"
-                    placeholder="your super secret password"
+                  <FormField
+                    control={control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="your super secret password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
-                </div>
-              </div>
-            </CardContent>
-            <CardFooter className="flex flex-col">
-              <Button onClick={joinRoom} className="w-full mt-2">
-                Log in
-              </Button>
-            </CardFooter>
-          <div className="w-full px-6">
+                </CardContent>
+                <CardFooter className="">
+                  <Button
+                    disabled={!isDirty || isLoading || isSubmitting}
+                    type="submit"
+                    className="w-full"
+                  >
+                    Login
+                  </Button>
+                </CardFooter>
+              </form>
+            </Form>
+          <div className="mx-10 mb-2 py-2">
             <Button
               onClick={async () => {
                 await signIn("github", {
                   callbackUrl: "/home",
                 });
               }}
-              className="w-full my-4"
+              className="w-full mt-4"
             >
               <GitHubLogoIcon />
-              <span className="ml-2">Github</span>
+              <p className="ml-2">GitHub</p>
             </Button>
             <Button
               onClick={async () => {
@@ -127,20 +170,21 @@ export default function Login() {
                   callbackUrl: "/home",
                 });
               }}
-              className="w-full my-4"
+              className="w-full mt-4"
             >
               <Image
-                width={24}
                 height={24}
+                width={24}
                 src={GoogleLogoIcon}
                 alt="google logo"
               />
-              <span className="ml-2">Google</span>
+              <p className="ml-2">Google</p>
             </Button>
           </div>
         </Card>
-        <Button className="" variant={"link"} onClick={() => router.push("/signup")}>
-          Don&apos;t have an account ?
+
+        <Button variant={"link"} onClick={() => router.push("/signup")}>
+          Don&apos;t have an account?
         </Button>
         </div>
       </div>
